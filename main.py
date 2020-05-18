bcd21c93e3167c22437abdba0d93b6c2b429bc2b
William Redenbaugh <51929010+william-redenbaugh@users.noreply.github.com>
2020-05-17 20:58:30 -0700

    We got a rainbow program running
    Tested rainbow stuff, integrated rgb to hsv conversion for led animations

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import serial
 import messagedata_pb2
 import heaat_message_pb2
-
 import time
 import udp_matrix
+import colorsys
 
 # Setting up the serial device. If we are doing serial testing
 #ser = serial.Serial("COM4", 115200, serial.EIGHTBITS, serial.PARITY_NONE)
@@ -13,17 +13,17 @@ def test_matrix():
     # server_address_port = ('192.168.1.27', 4040)
     # client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     matrix = udp_matrix.LEDMatrix(64, 32)
+    while True:
+        for h in range(250):
+            for y in range(32):
+                for x in range(64):
+                    hue_val = (x * 4 + h) % 255
+                    matrix.set_led_hsv(x, y, hue_val, 128, 250)
 
-    matrix.set_led(0, 0, 100, 100, 100)
-    for x in range(64):
-        for y in range(32):
-            matrix.set_led(x, y, 200, 200, 200)
             matrix.update()
             time.sleep(0.01)
 
-    matrix.update()
     #matrix = udp_matrix.UDPMatrix(64, 32)
-    #matrix.set_led(20, 20, 20, 10, 10)
 
     '''
     instruction_bytearray = bytearray([0] * 20)
@@ -48,6 +48,5 @@ def test_matrix():
     # ser.write(msg)
     # hey_world = "hello world"
     # msg = bytes(hey_world, 'ascii');
-
 
 test_matrix()
--- udp_matrix.py
+++ udp_matrix.py
@@ -1,4 +1,5 @@
 import socket
+import colorsys
 
 class LEDMatrix:
     def __init__(self, x_size, y_size):
@@ -13,8 +14,31 @@ class LEDMatrix:
     def set_led(self, x, y, r, g, b):
         spot = (self.x_size * y + x) * 3
         self.out_arr[spot] = r
-        self.out_arr[spot + 1] = r
-        self.out_arr[spot + 2] = r
+        self.out_arr[spot + 1] = g
+        self.out_arr[spot + 2] = b
+
+    def set_led_hsv(self, x, y, h, s, v):
+        hue = h
+        if hue > 255:
+            hue = 255
+        if hue < 0:
+            hue = 0
+        saturation = s
+        if saturation > 255:
+            saturation = 255
+        if saturation < 0:
+            saturation = 0
+        value = v
+        if value > 255:
+            value = 255
+        if value < 0:
+            value = 0
+
+        r, g, b = colorsys.hsv_to_rgb(hue/255, saturation/255, value/255)
+        r = int(r * 255)
+        g = int(g * 255)
+        b = int(b * 255)
+        self.set_led(x, y, r, g, b)
 
     def update(self):
         # If we are using UDP sockets to send information
