a8778ff86af2dbb01a7bc85a4f1462f187dffc18
= <=>
2020-05-21 00:11:29 -0700

    Updated main file to better represent current test

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import heaat_message_pb2
 import time
 import udp_matrix
 import colorsys
+import pyaudio
 
 # Setting up the serial device. If we are doing serial testing
 #ser = serial.Serial("COM4", 115200, serial.EIGHTBITS, serial.PARITY_NONE)
+# If we are doing serial testing
+# ser.write(msg)
+# hey_world = "hello world"
+# msg = bytes(hey_world, 'ascii');
 
-def test_matrix():
+def test_matrix_hsv():
     # Address and port for our esp32. if we are using sockets.
 
     server_address_port = ('192.168.1.27', 4040)
@@ -17,36 +22,9 @@ def test_matrix():
         for h in range(255):
             for y in range(32):
                 for x in range(64):
-                    hue_val = (y * x + h * 3) % 255
-                    matrix.set_led_hsv(x, y, hue_val, 128, 100)
+                    hue_val = ((y * x)/20 + h * 3) % 255
+                    matrix.set_led_hsv(x, y, hue_val, 255, 60)
             matrix.update()
             time.sleep(0.01)
 
-
-    #matrix = udp_matrix.UDPMatrix(64, 32)
-
-    '''
-    instruction_bytearray = bytearray([0] * 20)
-    # Setting up message and general instruction data to tell led to toggle led.
-    message_data = messagedata_pb2.MessageData()
-    message_data.message_size = len(instruction_bytearray)
-    message_data.message_type = messagedata_pb2.MessageData.MessageType.MATRIX_DATA
-
-    msg_dat = bytearray(message_data.SerializeToString())
-    msg_len = 16 - len(msg_dat)
-
-    # To ensure that our first message is always 16 bytes we append whatever remainder bytes
-    # are left.
-    for i in range(msg_len):
-        msg_dat.append(0)
-
-    # Then we tack on the last instruction
-    msg = msg_dat + instruction_bytearray
-    '''
-
-    # If we are doing serial testing
-    # ser.write(msg)
-    # hey_world = "hello world"
-    # msg = bytes(hey_world, 'ascii');
-
-test_matrix()
+test_matrix_hsv()
